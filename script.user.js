f9816ea8c0558fd3e1a63bac9d4fd221b5fc22bd
Asustv123 <112798924+Asustv123@users.noreply.github.com>
2026-05-19 14:04:52 +0700

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Bypass UptoLink (NekoVCheat)
-// @namespace    https://vanhvcheat.rf.gd/
+// @namespace    https://vanhvcheat.rf.gd
 // @version      2.0
 // @description  Bypass UptoLink — host trên nekovcheat.io
 // @require      https://raw.githubusercontent.com/Asustv123/Vietanhvcheat/main/source.js
 // @author       NekoVCheat
 // @grant        GM_xmlhttpRequest
 // @connect      uptolink.one
-// @connect      vanhvcheat.rf.io
+// @connect      vanhvcheat.rf.gd
 // @connect      api.kolosal.ai
 // @connect      *
 // @run-at       document-end
